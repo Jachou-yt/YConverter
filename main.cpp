21f12e355af3da6b81e82bef4ebca5356312f419
Jachou <matisse.martayan@gmail.com>
2023-06-30 23:38:12 +0200

    Add another libs

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+#include <exception>
 #include <Windows.h>
 
 std::string GetCurrentDirectory()
 {
     char buffer[MAX_PATH];
-    GetModuleFileNameA(NULL, buffer, MAX_PATH);
+    GetModuleFileNameA(nullptr, buffer, MAX_PATH);
     std::string::size_type pos = std::string(buffer).find_last_of("\\/");
 
     return std::string(buffer).substr(0, pos);
@@ -13,6 +14,7 @@ std::string GetCurrentDirectory()
 
 int main() {
     std::string url;
+    bool logs = false;
 
     std::cout << "Please enter youtube URL : ";
     std::getline(std::cin, url);
@@ -38,16 +40,35 @@ int main() {
         std::cout << "Valid file destination" << std::endl;
     }
 
-    // Download the video
-    std::string command = GetCurrentDirectoryA() + "/youtube-dl.exe --ignore-errors -o \"" + destination + "/%(title)s.%(ext)s\" " + url;
-    system(command.c_str());
+    std::string verbose;
+    std::cout << "Do you want to see the logs ? (y/n) : ";
+    std::getline(std::cin, verbose);
 
-    // Check if the video is downloaded
-    if (destination.find(destination) == std::string::npos) {
-        std::cout << "Video downloaded" << std::endl;
+    if (verbose == "y") {
+        logs = true;
+    } else if (verbose == "n") {
+        logs = false;
     } else {
-        std::cout << "Video not downloaded" << std::endl;
+        std::cout << "Invalid answer" << std::endl;
+        return 0;
     }
+
+    // Download the video
+    try {
+        if (logs) {
+            std::cout << "Downloading..." << std::endl;
+            std::string command = GetCurrentDirectoryA() + "/yt-dlp_min.exe --verbose --ignore-errors -o \"" + destination + "/%(title)s.%(ext)s\" " + url;
+            system(command.c_str());
+        } else {
+            std::cout << "Downloading..." << std::endl;
+            std::string command = GetCurrentDirectoryA() + "/yt-dlp_min.exe --ignore-errors -o \"" + destination + "/%(title)s.%(ext)s\" " + url;
+            system(command.c_str());
+        }
+    } catch (std::exception& e) {
+        std::cout << "Error : " << e.what() << std::endl;
+    }
+
+    std::cout << "Download completed" << std::endl;
 
     return 0;
 }
